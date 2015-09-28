3eda6253efbb9d08d97d775290812d53a12e3b46
anasnakawa <anas.nakawa@gmail.com>
2015-09-28 19:38:21 +0400

    initial two-way working binding handler

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# knockout-froala
+## knockout-froala
+
 Knockout.js binding for Froala WYSIWYG HTML Rich Text Editor
+
+## Usage
+
+to enable Froala binding on a textarea, you need to provide the following binding handlers 
+
+* `froalaEditor`: the model observable behind the editor
+* `froalaEditorOptions`: a plain object or an observable that will hold all the options to initalize the editor
+
+```js
+// view model
+this.comments = ko.observable();
+this.editorOptions = {
+  // disable wrapping content with paragraphs
+  // instead <br> will be used
+  enter: $.FroalaEditor.ENTER_DIV,
+
+  // we like gray!
+  theme: 'gray',
+  toolbarButtons: [ 'bold', 'italic', 'underline' ]
+}
+```
+
+```html
+<textarea data-bind="value: comments, froalaEditor: comments, froalaEditorOptions: editorOptions"></textarea>
+```
+
+## License
+
+The `knockout-froala` project is under MIT license.
+
+Froala Editor has [3 different licenses](http://froala.com/wysiwyg-editor/pricing) for commercial use.
+For details please see [License Agreement](http://froala.com/wysiwyg-editor/terms).
--- src/knockout-froala.js
+++ src/knockout-froala.js
@@ -2,22 +2,89 @@
  * knockout binding for Froala Editor
  */
 
-(function( ko, $ ) { 'use strict';
+ko.bindingHandlers.froalaEditor = (function() { 'use strict';
+
+  // locals
+  var unwrap = ko.utils.unwrapObservable;
 
   /**
-   * binding definition
+   * initiate froala editor, listen to its changes
+   * and updates the underlying observable model
+   *
+   * @param {element} element
+   * @param {object} value
+   * @param {object} bindings
+   * @api public
    */
 
-	ko.binidingHandlers.froalaEditor = {
-    init: function( element, value, bindings, viewModel, context ) {
-      var $el = $( element );
-      var options = ko.unwrapObservable( value() );
+  function init( element, value, bindings ) {
+    var $el = $( element );
+    var model = value();
+    var options = unwrap( bindings() ).froalaEditorOptions || {};
 
-      $el.froalaEditor( options );
-    },
-    update: function( element, value, bindings, viewModel, context ) {
+    // initialize the editor
+    $el.froalaEditor( options );
 
+    // update underlying model whenever editor content changed
+    $el.on('froalaEditor.contentChanged', function (e, editor) {
+      if( ko.isWriteableObservable( model ) ) {
+        var editorValue = editor.html.get();
+        var current = model();
+        if(current !== editorValue) {
+          model( editorValue );
+        }
+      }
+    });
+
+    // cleanup editor, when dom node is removed
+    ko.utils.domNodeDisposal.addDisposeCallback( element, destroy( element ) )
+  }
+
+  /**
+   * update froala editor whenever underlying observable model
+   * is updated
+   *
+   * @param {element} element
+   * @param {object} value
+   * @api public
+   */
+
+  function update( element, value ) {
+    var $el = $( element );
+    var modelValue = unwrap( value() );
+    var editorInstance = $el.data( 'froala.editor' );
+    var editorValue = editorInstance.html.get();
+
+    // avoid any un-necessary updates
+    if( editorValue !== modelValue ) {
+      editorInstance.html.set( modelValue );
     }
   }
 
-})( window.ko, window.jQuery );
+  /**
+   * destroy froala editor instance
+   *
+   * @param {dom} element
+   * @return {function} handler
+   * @api private
+   */
+
+  function destroy( element ) {
+    var $el = $( element );
+    return function() {
+      if( $el.data( 'froala.editor' ) ) {
+        $el.froalaEditor( 'destroy' );
+      }
+    }
+  }
+
+  /**
+   * expose `froala` binding handler methods
+   */
+
+  return {
+    init: init,
+    update: update
+  }
+
+})();
