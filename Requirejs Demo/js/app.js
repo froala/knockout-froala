19f1e3a3414efbe473951b845fb63d09bdcc96fa
Ravi Sharma <ravi.sharma@accoliteindia.com>
2019-04-17 22:08:31 +0530

    update Require js demo

--- Requirejs Demo/js/app.js
+++ Requirejs Demo/js/app.js
@@ -3,7 +3,7 @@
 requirejs(["knockout"],function(ko)
 {
 window.ko=ko;
-requirejs(["FroalaEditor"],function(FroalaEditor)
+requirejs(["../froala_editor.min"],function(FroalaEditor)
 {
  window.FroalaEditor = FroalaEditor;
 requirejs(["knockout-froala"],function()
